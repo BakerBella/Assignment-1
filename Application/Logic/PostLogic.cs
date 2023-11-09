40bff15ba99956ae604eaccd1dd8059c3fa3e74f
BakerBella <131524741+BakerBella@users.noreply.github.com>
2023-11-09 12:02:02 +0100

    Add files via upload

---
+++ Application/Logic/PostLogic.cs
@@ -0,0 +1,57 @@
+﻿using Application.DaoInterfaces;
+using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+
+namespace Application.Logic;
+
+public class PostLogic : IPostLogic
+{
+    private readonly IPostDao postDao;
+    private readonly IUserDao userDao;
+
+    public PostLogic(IPostDao postDao, IUserDao userDao)
+    {
+        this.postDao = postDao;
+        this.userDao = userDao;
+    }
+
+    public async Task<Post> CreateAsync(PostCreationDto dto)
+    {
+        User? user = await userDao.GetByIdAsync(dto.OwnerId);
+        if (user == null)
+        {
+            throw new Exception($"User with id {dto.OwnerId} was not found.");
+        }
+
+        Post post = new Post(user, dto.Title, dto.Body);
+
+        ValidatePost(post);
+
+        Post created = await postDao.CreateAsync(post);
+        return created;
+    }
+
+    public Task<IEnumerable<Post>> GetAsync(SearchPostParametersDto searchParameters)
+    {
+        return postDao.GetAsync(searchParameters);
+    }
+    
+    public async Task<PostBasicDto> GetByIdAsync(int id)
+    {
+        Post? post = await postDao.GetByIdAsync(id);
+        if (post == null)
+        {
+            throw new Exception($"Post with id {id} not found");
+        }
+
+        return new PostBasicDto(post.Id, post.Owner.UserName, post.Title, post.Body);
+    }
+    
+    private void ValidatePost(Post dto)
+    {
+        if (string.IsNullOrEmpty(dto.Title)) throw new Exception("Title cannot be empty.");
+        if (string.IsNullOrEmpty(dto.Body)) throw new Exception("Body cannot be empty.");
+        // other validation stuff
+    }
+}
