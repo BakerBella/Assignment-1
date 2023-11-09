07c5938e83dcd7a122b2760341d092a15fc9ce2a
BakerBella <131524741+BakerBella@users.noreply.github.com>
2023-11-09 12:10:39 +0100

    Add files via upload

---
+++ Web API/Controllers/PostsController.cs
@@ -0,0 +1,67 @@
+using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PostsController : ControllerBase
+{
+
+    private readonly IPostLogic postLogic;
+
+    public PostsController(IPostLogic postLogic)
+    {
+        this.postLogic = postLogic;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Post>> CreateAsync(PostCreationDto dto)
+    {
+        try
+        {
+            Post created = await postLogic.CreateAsync(dto);
+            return Created($"/posts/{created.Id}", created);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Post>>> GetAsync([FromQuery] string? userName, [FromQuery] int? userId,
+        [FromQuery] string? titleContains, [FromQuery] string? bodyContains)
+    {
+        try
+        {
+            SearchPostParametersDto parameters = new(userName, userId, titleContains, bodyContains);
+            var todos = await postLogic.GetAsync(parameters);
+            return Ok(todos);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<PostBasicDto>> GetById([FromRoute] int id)
+    {
+        try
+        {
+            PostBasicDto result = await postLogic.GetByIdAsync(id);
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+
+}
