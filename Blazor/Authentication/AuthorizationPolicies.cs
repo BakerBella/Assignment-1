5a41460a9651d01ecdf33728d4befb27b0b54247
BakerBella <131524741+BakerBella@users.noreply.github.com>
2023-11-09 12:13:10 +0100

    Add files via upload

---
+++ Blazor/Authentication/AuthorizationPolicies.cs
@@ -0,0 +1,17 @@
+namespace Blazor.Authentication;
+
+public static class AuthorizationPolicies
+{
+    public static void AddPolicies(IServiceCollection services)
+    {
+        services.AddAuthorizationCore(options =>
+        {
+    
+            options.AddPolicy("SecurityLevel2", a =>
+                a.RequireAuthenticatedUser().RequireClaim("SecurityLevel", "2"));
+    
+            options.AddPolicy("SecurityLevel1", a =>
+                a.RequireAuthenticatedUser().RequireClaim("SecurityLevel", "1", "2"));
+        });
+    }
+}
